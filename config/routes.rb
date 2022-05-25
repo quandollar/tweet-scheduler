1ac408fc38537575b0d68823018fde5a47a26fa0
Quan Nguyen <quanwnn@gmail.com>
2022-05-25 07:45:23 -0400

    add sessions controller and logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do  
+  
   root "pages#index"
+  
   get "about", to: "pages#about"
+  
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+  delete "sign_out", to: "sessions#destroy"
+
 end
